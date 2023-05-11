38f6315e50ceaf34621e6172083bae7786e0a5b8
Souhair <souhair.msokar@gmail.com>
2023-05-11 16:05:17 +0300

    Update client.sh

--- vms/client/simulator/client.sh
+++ vms/client/simulator/client.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
-# script to create the First virtual machine
-
-echo -e "-Create a server dirctory and create app.py inside it\n and install flask and tcpdump\nand run bash again\n" 
+# script to create the third virtual machine
+ip link add macvlan1 link eth0 type macvlan mode bridge
+ip address add dev macvlan1 192.168.4.100/24
+ip link set macvlan1 up
+ip route add 192.168.18.0/24 via 192.168.4.1 
 while [ true ] ; do
 read -t 3 -n 1
 if [ $? = 0 ] ; then
-break;
+break ;
 else
-echo "waiting for the keypress" 
+echo "waiting for the keypress"
 fi
 done
-ip link add macvlan1 link eth0 type macvlan mode bridge
-ip address add dev macvlan1 192.168.18.10/24
-ip link set macvlan1 up 
-ip route add 192.168.4.0/24 via 192.168.18.1 
